544e4a9e106cf5d05b0a4e9c1decfe4e08346498
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2024-10-01 09:01:53 +0000

    Day3 String

--- Day3/string.py
+++ Day3/string.py
@@ -1,14 +1,20 @@
 name: str = "winTer is Coding"
 
-print(f"normal string = {name}")
-print(f"lower case string = {name.lower()}")
-print(f"upper case string = {name.upper()}")
-print(f"capitalize case string = {name.capitalize()}")
-print(f"isalpha case string = {name.isalpha()}")
-print(f"isalpha case string = {name.title()}")
+# print(f"normal string = {name}")
+# print(f"lower case string = {name.lower()}")
+# print(f"upper case string = {name.upper()}")
+# print(f"capitalize case string = {name.capitalize()}")
+# print(f"isalpha case string = {name.isalpha()}")
+# print(f"isalpha case string = {name.title()}")
 
-# 
-print(f"number of i in the string = {name.count("w")}")
+# # 
+# print(f"number of i in the string = {name.count("w")}")
 
-print(f"if string ends with g = {name.endswith("Coding")}")
-print(f"if string starts with w = {name.startswith("w")}")
+# print(f"if string ends with g = {name.endswith("Coding")}")
+# print(f"if string starts with w = {name.startswith("w")}")
+
+# slice
+print(f"string slice = {name[2:8]}")
+
+# +  0  1  2  
+# - -3 -2 -1
