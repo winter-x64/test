4237d3747c7aad7f4ba761ff1d041125cba26156
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2024-10-01 09:58:36 +0000

    Day 3 complete

---
+++ Day3/conditions.py
@@ -0,0 +1,67 @@
+'''
+
+
+if condition:
+    do something
+
+    
+if condition:
+    do something
+else:
+    do something
+    
+
+if condition:
+    do something
+elif condition:
+    do something
+elif condition:
+    do something
+else:
+    do something
+
+
+'''
+
+# number_1: int = int(input("Enter number 1: "))
+# number_2: int = int(input("Enter number 2: "))
+
+# if number_1 > number_2:
+#     print(f"{number_1} is greater")
+
+# elif number_1 == number_2:
+#     print("Both are same")
+
+# else:
+#     print(f"{number_2} is greater")
+
+
+'''
++ - * \
+
+> < >= <= == !=
+
+
+= is assign
+x = 3
+
+condition
+x == y
+
+logical
+or and not
+|| && !
+'''
+
+running = False
+
+if not running:
+    print("Running....")
+
+
+# prof_name
+# prof_age
+# prof_gender
+
+
+# student_name
