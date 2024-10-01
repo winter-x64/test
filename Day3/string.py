3fca9478fbb8020c526cd48b371b76549194a472
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2024-10-01 03:29:19 +0000

    Day3

---
+++ Day3/string.py
@@ -0,0 +1,14 @@
+name: str = "winTer is Coding"
+
+print(f"normal string = {name}")
+print(f"lower case string = {name.lower()}")
+print(f"upper case string = {name.upper()}")
+print(f"capitalize case string = {name.capitalize()}")
+print(f"isalpha case string = {name.isalpha()}")
+print(f"isalpha case string = {name.title()}")
+
+# 
+print(f"number of i in the string = {name.count("w")}")
+
+print(f"if string ends with g = {name.endswith("Coding")}")
+print(f"if string starts with w = {name.startswith("w")}")
